568d7819a8af1dc345e52b1e07cb8480a43b6180
Nicole Nunez-Sainz <nnunezsa@ucla.edu>
2023-07-27 11:25:07 -0700

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Nicole Nunez on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
